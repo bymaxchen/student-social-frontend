a98a72d8a00023285bbe9330f491906e691e00c0
Julia Ekeblad <julia.ekeblad@gmail.com>
2024-02-03 23:02:13 +0100

    Signup page progress

--- src/pages/signUpPage/signUpPage.js
+++ src/pages/signUpPage/signUpPage.js
@@ -1,8 +1,9 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import { AuthContext } from '../../AuthContext'
-import { Form, Input, Button, Card } from 'antd';
+import { Form, Input, Button, Card, DatePicker, Radio } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
 import ChalmersLogo from '../../asserts/images/sign-up.svg'
 
 import './signUpPage.css';
@@ -19,74 +20,41 @@ function SignUpPage() {
     <div className="signup-container">
         <div className="form-container-sign-up">
           <div className='form-section-sign-up'>
+            <h1 className='form-header-signup'>Sign Up</h1>
             <Form
-              name="signup"
-              className="signup-form"
-              initialValues={{ remember: true }}
-              // onFinish={onFinish}
+                layout='vertical'
+                name="signup"
+                className="signup-form"
+                initialValues={{ remember: true }}
+                //onFinish={onFinish}
             >
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="Username or email" 
-              />
+            
+            <Form.Item className="signup-form-item" label="CID" required tooltip="This is a required field"          >
+              <Input placeholder="CID" />
             </Form.Item>
-            <Form.Item
-              name="cid"
-              rules={[{ required: true, message: 'Please input your CID!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="CID" 
-              />
+            <Form.Item className="signup-form-item" label="Username" required tooltip="This is a required field"          >
+              <Input placeholder="Username" />
             </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[{ required: true, message: 'Please input your Password!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="Password" 
-              />
+            <Form.Item className="signup-form-item" label="First Name" required tooltip="This is a required field"          >
+              <Input placeholder="First Name" />
             </Form.Item>
-            <Form.Item
-              name="password_repeat"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="Confirm password" 
-              />
+            <Form.Item className="signup-form-item" label="Last Name" required tooltip="This is a required field"          >
+              <Input placeholder="Last Name" />
             </Form.Item>
-            <Form.Item
-              name="firstname"
-              rules={[{ required: true, message: 'Please input your first name!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="First name" 
-              />
+            <Form.Item className="signup-form-item" label="Gender" required tooltip="This is a required field"          >
+                <Radio.Group>
+                    <Radio.Button value="female">Female</Radio.Button>
+                    <Radio.Button value="male">Male</Radio.Button>
+                </Radio.Group>
             </Form.Item>
-            <Form.Item
-              name="lastname"
-              rules={[{ required: true, message: 'Please input your last name!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="Last name" 
-              />
+            <Form.Item className="signup-form-item" label="Birthday" required tooltip="This is a required field"          >
+              <DatePicker />
             </Form.Item>
-            <Form.Item
-              name="birthday"
-              rules={[{ required: true, message: 'Please input your birthday!' }]}
-            >
-              <Input 
-                prefix={<UserOutlined className="site-form-item-icon" />} 
-                placeholder="Birthday" 
-              />
+            <Form.Item className="signup-form-item" label="Password" required tooltip="This is a required field"          >
+              <Input placeholder="Password" />
+            </Form.Item>
+            <Form.Item className="signup-form-item" label="Confirm Password" required tooltip="This is a required field"          >
+              <Input placeholder="Confirm Password" />
             </Form.Item>
             </Form>
           </div>
