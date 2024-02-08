977f1cda85cc54c66e336fa7e1e3b89db5857a28
TUKTUK77 <adrians.glazers@gmail.com>
2024-02-08 17:21:01 +0100

    made sum changes
    Co-authored-by: Julia Ekeblad <julia.ekeblad@gmail.com>

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -6,9 +6,12 @@ import PopUp from 'reactjs-popup';
 // import 'reactjs-popup/dist/index.css';
 import { Form, Input, Button, TextArea } from 'antd';
 
+
 function HomePage() {
   const { logout } = useContext(AuthContext);
-
+  const [title, setTitle] = useState('');
+  const [post, setPost] = useState('');
+  
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -27,47 +30,51 @@ function HomePage() {
   };
 
   const PostPopUp = () => (
-    <PopUp trigger={<Button>Create Post</Button>} modal >
-      <div className='popup'>
-        <div className='popup-header'>
-          <div className='popup-header-username'>
-            <h2>
-              username
-            </h2>
-          </div>
-          <div className='popup-header-close-button'>
-            <Button>
-              X
-            </Button>
-          </div>
-        </div>
-        <div>
-          <Form
-            layout='vertical'
-            name='post'
-            className='post-form'
-            initialValues={{ remember: true }}
-            // onFinish={whatever}
-          >
-            <Form.Item>
-              <Input placeholder='Title'/>
-            </Form.Item>
-            <Form.Item>
-              <Input.TextArea className='text_area' placeholder='Content' autoSize={{minRows: 5, maxRows :20}} showCount maxLength={10000}/>
-            </Form.Item>
-            <Form.Item>
-              <Button>
-                Add attachement
+    <PopUp trigger={<Button>Create Post</Button>} modal>
+      {
+        close => (
+          <div className='popup'>
+          <div className='popup-header'>
+            <div className='popup-header-username'>
+              <h2>
+                username
+              </h2>
+            </div>
+            <div className='popup-header-close-button'>
+              <Button onClick={() => close()}>
+                X
               </Button>
-              <Button>
-                POST!!!
-              </Button>
-            </Form.Item>
-          </Form>
+            </div>
+          </div>
+          <div>
+            <Form
+              layout='vertical'
+              name='post'
+              className='post-form'
+              initialValues={{ remember: true }}
+              // onFinish={whatever}
+            >
+              <Form.Item>
+                <Input placeholder='Title' value={title} onChange={e => setTitle(e.target.value)}/>
+              </Form.Item>
+              <Form.Item>
+                <Input.TextArea value={post} className='text_area' onChange={e => setPost(e.target.value)} placeholder='Content' autoSize={{minRows: 5, maxRows :20}} showCount maxLength={10000}/>
+              </Form.Item>
+              <Form.Item>
+                <Button>
+                  Add attachement
+                </Button>
+                <Button>
+                  POST!!!
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+  
         </div>
-
-      </div>
-
+  
+          )
+      }
     </PopUp>
 
   )
